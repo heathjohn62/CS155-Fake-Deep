1c6dbeafd551134e0e44c4684125b95caec4481c
ayooluwaodemuyiwa <40897521+ayooluwaodemuyiwa@users.noreply.github.com>
2021-02-08 13:25:29 -0600

    added create_train_test

--- project1/data_cleaning/data_cleaning.py
+++ project1/data_cleaning/data_cleaning.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# remove outliers with IQR 
+
 def IQR (d_in, lower_bound, upper_bound):
     # finding outlier functions first by getting the first and second quartile
     # of our data -> can update lower and upper bound as needed
@@ -11,3 +13,13 @@ def IQR (d_in, lower_bound, upper_bound):
     d_out = d_in[~((d_in < (Q1 - 1.5 * IQR)) |(d_in > (Q3 + 1.5 * IQR))).any(axis=1)]
     d_out.shape
     return d_out
+
+# create training set and test set
+def create_train_test(in_data, split_ratio):
+  # input .7 into split_ratio to get 70/30 split
+  # ind = values to get indices of set of set -> this is an array of bool values
+  ind = numpy.random.rand(len(in_data)) < split_ratio
+  tr_set = in_data[ind]
+  test_set = in_data[~ind]
+  #returns training set and test set as dataframes 
+  return tr_set, test_set
